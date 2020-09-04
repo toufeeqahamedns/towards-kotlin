0bf7d4719514a40bed35f266b001531c32470cbd
toufeeqahamedns <toufeeqahamedns@gmail.com>
2020-09-04 18:39:02 +0530

    Basic Fucntionality

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.widget.Button
+import android.widget.Toast
+import com.example.diceroller.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+//        findViewById will be deprecated use viewBinding
+
+//        val rollButton: Button = findViewById(R.id.roll_button)
+//        rollButton.text = "Let's Roll"
+
+        binding.rollButton.text = "Let's Roll"
+
+        binding.root.setVerticalGravity(Gravity.CENTER_VERTICAL)
+
+        binding.rollButton.setOnClickListener {
+            Toast.makeText(this, "Button Clicked", Toast.LENGTH_SHORT).show()
+            binding.resultText.text = (Random.nextInt(6) + 1).toString()
+        }
     }
 }
