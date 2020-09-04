5f4036a699b4729286fce0b040a84d73685ee935
toufeeqahamedns <toufeeqahamedns@gmail.com>
2020-09-04 19:29:08 +0530

    Added Dice Images(Vectors)

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Gravity
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.Toast
 import com.example.diceroller.databinding.ActivityMainBinding
 import kotlin.random.Random
@@ -11,6 +12,8 @@ import kotlin.random.Random
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    lateinit var diceImage: ImageView
+    lateinit var rollButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,14 +24,31 @@ class MainActivity : AppCompatActivity() {
 
 //        val rollButton: Button = findViewById(R.id.roll_button)
 //        rollButton.text = "Let's Roll"
-
-        binding.rollButton.text = "Let's Roll"
-
         binding.root.setVerticalGravity(Gravity.CENTER_VERTICAL)
+
+        diceImage = binding.diceImage
+        rollButton = binding.rollButton
+
+        rollButton.text = "Let's Roll"
 
         binding.rollButton.setOnClickListener {
             Toast.makeText(this, "Button Clicked", Toast.LENGTH_SHORT).show()
-            binding.resultText.text = (Random.nextInt(6) + 1).toString()
+//            binding.resultText.text = (Random.nextInt(6) + 1).toString()
+            rollDice()
+
         }
+    }
+
+    private fun rollDice() {
+        val drawableResource = when (Random.nextInt(6) + 1) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResource)
     }
 }
