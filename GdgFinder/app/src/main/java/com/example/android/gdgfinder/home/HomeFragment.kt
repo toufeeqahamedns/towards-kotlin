fb4a1ea5f8a0b9bdf42e7fc39ee69c1a44b5915a
toufeeqahamedns <toufeeqahamedns@gmail.com>
2020-11-12 20:04:27 +0530

    Adding styles

--- GdgFinder/app/src/main/java/com/example/android/gdgfinder/home/HomeFragment.kt
+++ GdgFinder/app/src/main/java/com/example/android/gdgfinder/home/HomeFragment.kt
@@ -5,8 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.findNavController
 import com.example.android.gdgfinder.R
+import com.example.android.gdgfinder.databinding.HomeFragmentBinding
 
 class HomeFragment : Fragment() {
 
@@ -23,6 +27,18 @@ class HomeFragment : Fragment() {
         val view = inflater.inflate(R.layout.home_fragment, container, false)
         viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
 
-        return view
+        val binding = HomeFragmentBinding.inflate(inflater)
+        viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
+        binding.viewModel = viewModel
+
+        viewModel.navigateToSearch.observe(viewLifecycleOwner, Observer { shouldNavigate ->
+            if (shouldNavigate == true) {
+                val navController = binding.root.findNavController()
+                navController.navigate(R.id.action_homeFragment_to_gdgListFragment)
+                viewModel.onNavigatedToSearch()
+            }
+        })
+
+        return binding.root
     }
 }
